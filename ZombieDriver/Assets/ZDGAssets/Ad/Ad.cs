883c62b7dd8b0b30d4bf9d38bb2698ac3c288ce3
CZY <chenzhengyoung@gmail.com>
2025-01-13 19:04:11 +0800

    1.0

---
+++ ZombieDriver/Assets/ZDGAssets/Ad/Ad.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZombieDriveGame;
+
+
+
+public class Ad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void ClickBtn()
+    {
+        AdManager.ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
+                    if (ZDGGameController.instance.playerObject.health <= 0 && ZDGGameController.instance.playerObject.fuel <= 0)
+                    {
+                        ZDGGameController.instance.ChangeHealth(ZDGGameController.instance.playerObject.healthMax);
+                        ZDGGameController.instance.ChangeFuel(ZDGGameController.instance.playerObject.fuelMax);
+                    }
+                    else if (ZDGGameController.instance.playerObject.health<=0 && ZDGGameController.instance.playerObject.fuel > 0)
+                    {
+                        ZDGGameController.instance.ChangeHealth(ZDGGameController.instance.playerObject.healthMax);
+                    }
+                    else if(ZDGGameController.instance.playerObject.fuel<=0 && ZDGGameController.instance.playerObject.health > 0)
+                    {
+                        ZDGGameController.instance.ChangeFuel(ZDGGameController.instance.playerObject.fuelMax);
+                    }
+                    ZDGGameController.instance.StopCoroutines();
+                    ClosePanel();
+
+                    AdManager.clickid = "";
+                    AdManager.getClickid();
+                    AdManager.apiSend("game_addiction", AdManager.clickid);
+                    AdManager.apiSend("lt_roi", AdManager.clickid);
+
+
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+
+    }
+
+
+    public void ClosePanel()
+    {
+        ZDGGameController.instance.SetAdPanel(false);
+    }
+
+}
